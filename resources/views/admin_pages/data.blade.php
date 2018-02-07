f50dffad684c9692d8fd9beb3ae4085b91482d25
RULNJT0PZN1BSFU\Administrator <khalid.yahya9@gmail.com>
2018-02-08 03:03:49 +0700

    database

--- resources/views/admin_pages/data.blade.php
+++ resources/views/admin_pages/data.blade.php
@@ -10,7 +10,7 @@
     <meta name="author" content="">
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/img/apple.png') }}">
-    <title>Data</title>
+    <title>Data List</title>
     <!-- Bootstrap Core CSS -->
     <link href="{{ asset('admin/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
     <!-- Custom CSS -->
